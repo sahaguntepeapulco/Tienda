ab974437e5b778fe976dd8a9d70ccceb0259d484
sahaguntepeapulco <111219117+sahaguntepeapulco@users.noreply.github.com>
2023-09-18 13:24:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from flask import send_from_directory
 from dotenv import load_dotenv
 import uuid
-from github import Github
+
 
 
 load_dotenv()
@@ -18,11 +18,11 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 
 mysql = MySQL(app)
-app.config['MYSQL_DATABASE_USER'] = os.getenv("MYSQLUSER")
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("MYSQLPASSWORD")
-app.config['MYSQL_DATABASE_DB'] = os.getenv("MYSQLDATABASE")
-app.config['MYSQL_DATABASE_HOST'] = os.getenv("MYSQLHOST")
-app.config['MYSQL_DATABASE_PORT'] = int(os.getenv("MYSQLPORT")
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['MYSQL_DATABASE_HOST'] = 'bkpxwmspyhvlwbsedoix-mysql.services.clever-cloud.com'
+app.config['MYSQL_DATABASE_USER'] = 'ulp53jflirtiiq73'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'jGxGZjj8kmle7w7F5UWk'
+app.config['MYSQL_DATABASE_DB'] = 'bkpxwmspyhvlwbsedoix'
 
 
 mysql.init_app(app)
@@ -178,25 +178,6 @@ def admin_productos_guardar():
         nuevoNombre=horaActual+"_"+_archivo.filename
         _archivo.save("templates/sitio/img/"+nuevoNombre)
 
-        # Ahora guarda en GitHub
-        with open("templates/sitio/img/"+nuevoNombre, 'rb') as file:
-            content = file.read()
-
-        token = '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'
-        g = Github(token)
-        repo = g.get_repo("sahaguntepeapulco/Tienda")
-
-        # Intenta borrar el archivo si ya existe (opcional)
-        try:
-            repo.get_contents("templates/sitio/img/"+nuevoNombre).delete("remove old image")
-        except:
-            pass
-
-        # Ahora crea el nuevo archivo
-        repo.create_file("templates/sitio/img/"+nuevoNombre, "upload new image", content)
-
-
-
     sql = "INSERT INTO `productos` (`id`, `nombre`, `imagen`, `url`, `precio`) VALUES (NULL, %s, %s, %s, %s);"
 
     datos = (_nombre, nuevoNombre, _url, _precio)
@@ -243,3 +224,5 @@ def admin_productos_borrar():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
